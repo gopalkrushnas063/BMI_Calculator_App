1e09846566d90eb13b8d438c0401efa5c03e1a2d
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2024-01-04 17:39:27 +0530

    bmi bottom navigation added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,8 @@
-import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:bmi_calculator_app/bmi_history_page.dart';
 import 'package:bmi_calculator_app/bmi_home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,12 +12,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
- 
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    BMIHomePage(),
+    BMIHistoryPage(),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: BMIHomePage(),
+    return  Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: ConvexAppBar(
+        backgroundColor: Colors.lightGreen,
+        items: [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.assignment, title: 'BMI History'),
+        ],
+        onTap: (int index) {
+          // Implement your logic here based on the tapped index
+          setState(() {
+            _currentIndex =
+                index; // Assuming _currentIndex is a variable in your State
+          });
+
+          // You can add additional logic here based on the tapped index
+          switch (index) {
+            case 0:
+              // Logic for Home tab
+              break;
+            case 1:
+              // Logic for Discovery tab
+              break;
+            case 2:
+              // Logic for Add tab
+              break;
+            // Add more cases for additional tabs if needed
+          }
+        },
+        initialActiveIndex: _currentIndex, // Set initial active index
+      ),
     );
   }
 }
