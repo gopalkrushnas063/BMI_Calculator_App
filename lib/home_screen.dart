6994359d0738e55c66acd668fa8bad8d04d3e590
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2024-01-04 17:33:35 +0530

    bmi app updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,71 +11,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentPageIndex = 0;
-  final PageController _pageController = PageController(initialPage: 0);
-  late NotchBottomBarController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = NotchBottomBarController(index: _currentPageIndex);
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (index) {
-          setState(() {
-            _currentPageIndex = index;
-            _controller.index = index; // Update the controller index
-          });
-        },
-        children: [
-          const BMIHomePage(), // Removed 'const'
-          BMIHistoryPage(),
-        ],
-      ),
-      bottomNavigationBar: AnimatedNotchBottomBar(
-        notchBottomBarController: _controller,
-        color: Colors.white,
-        showLabel: false,
-        shadowElevation: 5,
-        kBottomRadius: 28.0,
-        notchColor: Colors.black87,
-        removeMargins: false,
-        bottomBarWidth: MediaQuery.of(context).size.width,
-        durationInMilliSeconds: 300,
-        bottomBarItems: const [
-          BottomBarItem(
-            inActiveItem: Icon(
-              Icons.home_filled,
-              color: Colors.blueGrey,
-            ),
-            activeItem: Icon(
-              Icons.home_filled,
-              color: Colors.blueAccent,
-            ),
-            itemLabel: 'Home',
-          ),
-          BottomBarItem(
-            inActiveItem: Icon(
-              Icons.history,
-              color: Colors.blueGrey,
-            ),
-            activeItem: Icon(
-              Icons.history,
-              color: Colors.blueAccent,
-            ),
-            itemLabel: 'History',
-          ),
-        ],
-        onTap: (index) {
-          _pageController.jumpToPage(index);
-        },
-        kIconSize: 24.0,
-      ),
+    return const Scaffold(
+      body: BMIHomePage(),
     );
   }
 }
